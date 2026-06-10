246a81f9ef19b8a5cc018c15bb0fdcf83d7664b2
Manish Bonde <manish.bonde11@gmai.com>
2026-06-10 15:31:12 +0530

    my second commit

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
 function add(a, b) {
   return a + b;
 }
+
+const value = add(5, 5);
+console.log(value);
--- four.jsx
+++ four.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const four = () => {
-  return <div></div>;
+  return (
+    <>
+      <h1>This is my first component</h1>
+    </>
+  );
 };
 
 export default four;
